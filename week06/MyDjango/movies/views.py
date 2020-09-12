0e69eadd1f88072f14d536b7e2d6a5ea468b9a2d
MJY <747523913@qq.com>
2020-09-12 23:41:02 +0800

    更新作业

--- week06/MyDjango/movies/views.py
+++ week06/MyDjango/movies/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import T1
 
-# Create your views here.
+
+def start_gt_3(request):
+    # 大于3星的
+    queryset = T1.objects.all()
+    condition = {'n_star__gt': 3}
+    shorts = queryset.filter(**condition)
+    # return HttpResponse('text')
+    return render(request, 'result.html', locals())
+
+
+def search_comment(request):
+    text = request.GET.get('q')
+    # 大于3星的
+    queryset = T1.objects.all()
+    condition = {'n_star__gt': 3, 'short__icontains': text}
+    shorts = queryset.filter(**condition)
+    # return HttpResponse('text')
+    return render(request, 'index.html', locals())
