36d4e61afd0a5453d3c3568a8ae8309d105b927f
MJY <747523913@qq.com>
2020-09-12 23:09:37 +0800

    更新作业

---
+++ week09/Login/index/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.my_home),
+    path('my_login', views.my_login),
+    path('my_logout', views.my_logout)
+]
